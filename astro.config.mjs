22e83d949aa87877f2e1df4230ae96b84ad40050
Roman Chursanov <authfailed@gmail.com>
2024-10-25 22:58:34 +0500

    Update for GhPages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://authfailed.github.io',
+    base: 'line-analyzer',
+});
