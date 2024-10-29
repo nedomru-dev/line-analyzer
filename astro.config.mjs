6c544b7a9dd6de7a74cce2c8e6b2cf03a4d5b452
Roman Chursanov <authfailed@gmail.com>
2024-10-29 09:10:39 +0500

    Removed base and site for GH Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://authfailed.github.io',
-    base: 'line-analyzer',
-});
+export default defineConfig({});
